397bdeaaf10a70b3585b95a5607ea83dcd3f4421
Terry Chai <terry.chai@oracle.com>
2017-04-25 15:25:29 -0700

    Add Authentication

--- android/app/src/main/java/com/luckydigits/android/MainActivity.java
+++ android/app/src/main/java/com/luckydigits/android/MainActivity.java
@@ -1,9 +1,28 @@
 package com.luckydigits.android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed, update UI appropriately
+                // ...
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
